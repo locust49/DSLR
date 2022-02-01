104e593f45008d4b67825b54035409d210205335
locust49 <lyazid.souad@gmail.com>
2022-02-01 23:53:07 +0100

    onevsall done -- needs restructuring

--- logistic_regression_class.py
+++ logistic_regression_class.py
@@ -1,42 +1,148 @@
 import numpy as np
+import pandas as pd
+
+from maths_class import Mathematics
+
+def write(this):
+	with open('./debug/file.txt', 'w+') as f :
+		f.write(this.toString())
 
 class LogisticRegression:
 
-	def __init__(self, learning_rate=None, epoch=None) -> None:
+	def __init__(self, learning_rate=None, epoch=None, bias=None) -> None:
 		self.learning_rate = 0.01 if learning_rate == None else learning_rate
 		self.epoch = 10000 if epoch == None else epoch
+		self.bias = True if bias == None else False
 
-	# y_predicted_value = self.hypothesis_matrix(X_matrix, bias, weights)
-	def loss(self, y_predicted_value, y_value) -> np.floating:
-		Y_class_0 = - np.log(y_predicted_value)
-		Y_class_1 = - np.log(1 - y_predicted_value)
+	def loss(self, y_predicted_value, y_value) -> np.float64:
 
-		loss_value = ((y_value * Y_class_0) + ((1 - y_value) * Y_class_1))
-		return loss_value
+		y_predicted_value.tofile('./debug/y_pred', '\n', '%.2f')
+		y_value.tofile('./debug/y_val', '\n', '%.2f')
 
-	def cost(self, sum_loss, number_of_x) -> np.floating:
-		return sum_loss / number_of_x
+		# print('y_predicted_value: ', type(y_predicted_value), y_predicted_value.shape)
+		# print('1 - y_predicted_value: ', 1 - y_predicted_value)
 
-	def cost_derivative_weights(Y_predicted_matrix, Y_matrix, X_matrix) -> np.matrix:
-		number_of_data = Y_matrix.shape[0]
-		return ((1 / number_of_data) * np.dot((Y_predicted_matrix - Y_matrix), X_matrix))
+		Y_class_1 =  np.log(y_predicted_value)
+		Y_class_0 =  np.log(1 - y_predicted_value)
+		Y_class_0.tofile('./debug/Y_class_0', '\n', '%.2f')
+		Y_class_1.tofile('./debug/Y_class_1', '\n', '%.2f')
+		# print('Y_class_0: ', type(Y_class_0), Y_class_0.shape, Y_class_0)
+		# print('Y_class_1: ', type(Y_class_1), Y_class_1.shape, Y_class_1)
 
-	def cost_derivative_bias(Y_predicted_matrix, Y_matrix) -> np.floating:
-		number_of_data = Y_matrix.shape[0]
+		# print('y_value: ', type(y_value), y_value.shape)
+
+
+		loss_value = (np.dot(y_value, Y_class_0) + (np.dot((1 - y_value), Y_class_1)))
+		# print('y_value: ', type(y_value), y_value.shape)
+
+		# print('loss_value ', np.float64(loss_value))
+		return np.float64(loss_value)
+
+
+	def cost_derivative_weights(self, Y_predicted_matrix, Y_matrix, X_matrix) -> np.matrix:
+		number_of_data = X_matrix.shape[0]
+
+		#print('Y_predicted_matrix: ', type(Y_predicted_matrix), Y_predicted_matrix.shape)
+		#print('Y_matrix: ', type(Y_matrix), Y_matrix.shape)
+		#print('(Y_predicted_matrix - Y_matrix).transpose(): ', type((Y_predicted_matrix.transpose() - Y_matrix).transpose()), (Y_predicted_matrix - Y_matrix).transpose().shape)
+
+		return ((1 / number_of_data) * np.dot((Y_predicted_matrix - Y_matrix).transpose() , X_matrix))
+
+	def cost_derivative_bias(self, Y_predicted_matrix, Y_matrix) -> np.floating:
+		number_of_data = Y_matrix.shape[1]
 		return ((1 / number_of_data) * (Y_predicted_matrix - Y_matrix))
 
+	def _hypothesis(self, X_matrix) -> np.floating:
+		X_value = np.dot(X_matrix, self.weights.transpose())
+		X_value_float = np.array(X_value, dtype=np.floating)
+		sig_val = self.sigmoid(X_value_float)
+		# print(sig_val)
+		# X_value_float.tofile('./debug/X_value_float', '\n', '%.2f')
+		sig_val.tofile('./debug/sig_val', '\n', '%.2f')
+		return (sig_val)
 
-	def hypothesis(self, X_matrix, bias, weights) -> np.matrix:
-		return (self.sigmoid(bias + np.dot(X_matrix, weights)))
+	def hypothesis(self, X_matrix, weights) -> np.floating:
+		print('w = {}, {}'.format(type(weights), weights))
+		X_value = np.dot(X_matrix, weights.transpose())
+		X_value_float = np.array(X_value, dtype=np.floating)
+		sig_val = self.sigmoid(X_value_float)
+		# print(sig_val)
+		# X_value_float.tofile('./debug/X_value_float', '\n', '%.2f')
+		sig_val.tofile('./debug/sig_val', '\n', '%.2f')
+		return (sig_val)
 
 	@staticmethod
 	def sigmoid(x_value) -> np.floating:
-		return 1 / 1 + np.exp(0 - x_value)
+		# # print('x_value: ', type(x_value), x_value.shape)
+		# s = np.sum(x_value)
+		# # print('s: ', type(s))
+
+		return (1 / (1 + np.exp(-x_value)))
+
+	def train(  self,
+				X_data: pd.DataFrame,
+				Y_data: pd.DataFrame,
+				verbose=False) -> None:
+
+		X_values_matrix = np.matrix(X_data.to_numpy())
+		Y_values_matrix = np.matrix(Y_data.to_numpy())
+		if self.bias == True:
+			x_bias  = np.ones((X_values_matrix.shape[0], 1))
+			X_values_matrix = np.concatenate((X_values_matrix, x_bias), axis=1)
+
+		total_data, total_weights = X_values_matrix.shape
+		self.weights = np.matrix([1.0 for i in range(total_weights)])
+
+		Y_predicted_matrix = self._hypothesis(X_values_matrix)
+		for _ in range(self.epoch):
+			#print('Y_predicted_matrix: ', type(Y_predicted_matrix), Y_predicted_matrix.shape)
+			#print('Y_values_matrix: ', type(Y_values_matrix), Y_values_matrix.shape)
+			gradient_weights = self.cost_derivative_weights(Y_predicted_matrix, Y_values_matrix.transpose(), X_values_matrix)
+			# print(self.weights.shape, gradient_weights.shape)
+			#print('weight: ', type(self.weights), self.weights.shape)
+			#print('gradient_weights: ', type(gradient_weights), gradient_weights.shape)
+			self.weights = self.weights - np.multiply(self.learning_rate, gradient_weights)
+
+			#print('X_values_matrix: ', type(X_values_matrix), X_values_matrix.shape)
+			Y_predicted_matrix = self._hypothesis(X_values_matrix)
+
+		print('Weights: ', type(self.weights), self.weights.shape)
 
 
-	def gradient_descent(self, X_matrix, Y_matrix):
-		pass
 
-	def train(self, X_matrix, Y_matrix) -> None:
+	def onevsall(self, X_values_matrix, Y_values_matrix, classes):
+		class_weight = { _class : None for _class in classes }
+		for index, class_name in enumerate(classes):
+			Y_select = Y_values_matrix.copy()
+			Y_select[Y_values_matrix == classes[index]] = 1
+			Y_select[Y_values_matrix != classes[index]] = 0
+			self.train(X_values_matrix, Y_select)
+			# print('loss shape = {}'.format(loss.shape))
+			class_weight[class_name] = self.weights.astype(np.float64)
+			# break
+		# print('costs = {}, max_cost = {} {}'.format(costs, type(np.max(costs)), np.max(costs)))
+		# index = costs.index(np.max(costs))
+		# print('index = {}'.format(index))
+		# self.weights = class_weight[index]
+		# self.weights.tofile('./debug/weights', '\n', '%.2f')
+		print('class_weight = {}'.format(class_weight))
+		return (class_weight)
 
-		self.gradient_descent()
+
+	def predict(self, X_values_matrix, class_weight):
+		if self.bias == True:
+			x_bias  = np.ones((X_values_matrix.shape[0], 1))
+			X_values_matrix = np.concatenate((X_values_matrix, x_bias), axis=1)
+
+		Y_fit = pd.DataFrame(index=range(0, X_values_matrix.shape[0]), columns=class_weight.keys())
+
+		for class_name in class_weight:
+			Y_fit[class_name] = self.hypothesis(X_values_matrix, class_weight[class_name])
+		Y_fit['pred'] = Y_fit.idxmax(axis=1)
+
+
+		Y_fit['pred'].to_csv('./debug/my_pred.csv')
+		# print(Y_fit)
+
+		# class_weight.tofile('./debug/result', '\n', '%f')
+		return(Y_fit['pred'])
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from re import L
+from statistics import mode
 from numpy import percentile
+from sklearn.metrics import accuracy_score
 from data_class import Data
+from logistic_regression_class import LogisticRegression
+import pandas as pd
+import numpy as np
+newData = Data.from_csv('./datasets/dataset_train.csv')
 
-newData = Data.from_csv('./datasets/dataset_test.csv')
-
-
+# pd.set_option("display.max_rows", None, "display.max_columns", None)
 # Testing each method
 # print('Count:\n' , newData.ft_count())
 # print('Min:\n' , newData.ft_min())
@@ -13,10 +18,68 @@ newData = Data.from_csv('./datasets/dataset_test.csv')
 # print('perc:\n' , newData.ft_percentiles([25, 50, 75]))
 
 # Testing the describe method
-my_desc = newData.ft_describe(percentiles = [.89, .40, .6])
-desc = newData.data.describe(percentiles = [.89, .40, .6])
+my_desc = newData.ft_describe(percentiles=[.25, .50, .75])
+desc = newData.data.describe()
 
 # Testing the describe method
-print('my describe:\n' , my_desc)
-print('em describe:\n' , desc)
-print('accuracy:\n', desc - my_desc)
+# print('my describe:\n' , my_desc)
+# print('em describe:\n' , desc)
+# print('accuracy:\n', desc - my_desc)
+
+model = LogisticRegression()
+
+
+################################ TRAIN ONEVSALL ################################
+df_train = pd.read_csv('./datasets/dataset_train.csv', index_col=0)
+fill = df_train.fillna(df_train.mean())
+
+data_x = fill.select_dtypes(np.number)
+df_norm = data_x / data_x.max()
+target_y = fill['Hogwarts House']
+
+print(df_norm)
+houses = ['Hufflepuff', 'Ravenclaw', 'Slytherin', 'Gryffindor']
+# w = model.onevsall(df_norm, target_y, houses)
+
+
+
+################################# TRAIN BINARY ################################
+# df_train = pd.read_csv('./datasets/dataset_train.csv', index_col=0)
+# fill = df_train.fillna(df_train.mean())
+
+# data_x = fill.select_dtypes(np.number)
+
+# target_y = fill[['Hogwarts House']]
+# target_y.loc[target_y['Hogwarts House'] == 'Gryffindor', 'Hogwarts House'] = 1
+# target_y.loc[target_y['Hogwarts House'] != 'Gryffindor', 'Hogwarts House'] = 0
+# print(target_y)
+
+
+# model.train(data_x, target_y)
+
+
+################################ PREDICT ################################
+
+
+
+df_predict = pd.read_csv('./datasets/dataset_test.csv', index_col=0)
+df_predict.dropna(axis=1, how='all', inplace=True)
+fill = df_predict.fillna(df_train.mean())
+data_x = fill.select_dtypes(np.number)
+df_norm = data_x / data_x.max()
+
+
+# pred = model.predict(df_norm, w)
+
+##################################### TEST #####################################
+
+df_test = pd.read_csv('./datasets/dataset_truth.csv', index_col=0)
+pred = pd.read_csv('./debug/my_pred.csv', index_col=0)
+
+df_test['pred'] = pred
+
+df_test['res'] = 0
+
+df_test['res'] = np.where(df_test['pred'] == df_test['Hogwarts House'] , 1, 0)
+print(df_test[df_test['res'] == 0])
+print('accuracy : ', df_test['res'].mean())
