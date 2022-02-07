31a445c437e398d514184f1e8d77366d6fbb95ab
locust49 <lyazid.souad@gmail.com>
2022-02-07 01:09:08 +0100

    add logreg_train file

---
+++ logreg_train.py
@@ -0,0 +1,45 @@
+import	numpy	as np
+import	pandas	as pd
+import	sys
+
+from logistic_regression_class	import LogisticRegression
+
+def print_usage():
+	pass
+
+def write_to_file(dictionary, filename):
+	try:
+		with open(filename, 'wt') as file:
+			file.write(str(dictionary))
+	except:
+		print("Unable to write to file : {}.".format(filename))
+
+
+def main():
+	if len(sys.argv) != 2 :
+		print_usage()
+	try:
+		model = LogisticRegression()
+
+		data = pd.read_csv(sys.argv[1], index_col=0)
+
+		# Features (X)
+		numerical_data = data.select_dtypes(include=[np.number])
+		features = numerical_data.fillna(numerical_data.mean())
+		features_normalized = (features - features.mean()) / features.std()
+
+		# Target (Y)
+		target = data['Hogwarts House']
+
+		# Classes (How many unique target)
+		classes = target.unique()
+
+		# Train and get results
+		weights = model.onevsall(features_normalized, target, classes)
+		write_to_file(weights, './weights')
+	except :
+		print('An error somewhere occured ! Reboot maybe ?')
+		pass
+
+if __name__ == '__main__' :
+	main()
