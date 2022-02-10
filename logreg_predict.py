66ad459876b3f880f4072ab3c1acf6fc3b6aedde
locust49 <lyazid.souad@gmail.com>
2022-02-10 21:18:10 +0100

    remove cleaning from main

--- logreg_predict.py
+++ logreg_predict.py
@@ -8,43 +8,23 @@ from logistic_regression_class	import LogisticRegression
 def print_usage():
 	exit(0)
 
-def write_to_file(dictionary, filename):
-	try:
-		with open(filename, 'wt') as file:
-			file.write(str(dictionary))
-	except:
-		print("Unable to write to file : {}.".format(filename))
-
 def main():
 	if len(sys.argv) != 4 :
 		print_usage()
 
-	model = LogisticRegression(bias=False, verbose=True)
+	model = LogisticRegression()
 
 	data = pd.read_csv(sys.argv[1], index_col=0)
 	weights = pd.read_csv(sys.argv[2], index_col=0)
 	target = pd.read_csv(sys.argv[3], index_col=0)
 
 	data.dropna(axis=1, how='all', inplace=True)
-	numerical_data = data.select_dtypes(include=[np.number])
 
-	data_x = numerical_data.fillna(numerical_data.mean())
-
-	features = (data_x - data_x.mean()) / data_x.std()
-
-
-
-	result = model.predict(features, weights)
+	result = model.predict(data, weights)
 	result.to_csv('./houses.csv')
 
-
 	accuracy = model.calculate_accuracy(target, result)
-	print('target = ', target)
-	print('result = ', result)
 	print('Accuracy = ', accuracy)
-
-
-
 
 if __name__ == '__main__' :
 	main()
--- logreg_train.py
+++ logreg_train.py
@@ -7,39 +7,26 @@ from logistic_regression_class	import LogisticRegression
 def print_usage():
 	exit(0)
 
-def write_to_file(dictionary, filename):
-	try:
-		with open(filename, 'wt') as file:
-			file.write(str(dictionary))
-	except:
-		print("Unable to write to file : {}.".format(filename))
-
-
 def main():
-	if len(sys.argv) != 2 :
-		print_usage()
-	try:
-		model = LogisticRegression(bias=False, verbose=True)
+	# if len(sys.argv) != 2 :
+	# 	print_usage()
+	# try:
+	model = LogisticRegression(fillna='drop')
 
-		data = pd.read_csv(sys.argv[1], index_col=0)
+	data = pd.read_csv(sys.argv[1], index_col=0)
 
-		# Features (X)
-		numerical_data = data.select_dtypes(include=[np.number])
-		features = numerical_data.fillna(numerical_data.mean())
-		features_normalized = (features - features.mean()) / features.std()
+	# Target (Y)
+	target = data['Hogwarts House']
 
-		# Target (Y)
-		target = data['Hogwarts House']
+	# Classes (How many unique target)
+	classes = target.unique()
 
-		# Classes (How many unique target)
-		classes = target.unique()
-
-		# Train and get results
-		weights = model.onevsall(features_normalized, target, classes)
-		weights.to_csv('./weights.csv')
-	except :
-		print('An error somewhere occured ! Reboot maybe ?')
-		pass
+	# Train and get results
+	weights = model.onevsall(data, target, classes)
+	weights.to_csv('./weights.csv')
+	# except :
+	# 	print('An error somewhere occured ! Reboot maybe ?')
+	# 	pass
 
 if __name__ == '__main__' :
 	main()
