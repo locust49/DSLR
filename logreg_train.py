951eed258210d9d1f8a0a9feaa638a6c2d9652c7
locust49 <lyazid.souad@gmail.com>
2022-02-12 23:00:20 +0100

    error management train

--- logreg_train.py
+++ logreg_train.py
@@ -2,30 +2,31 @@ import	pandas	as pd
 import	sys
 
 from logistic_regression_class	import LogisticRegression
+from logreg_error_management    import *
 
-def print_usage():
-	exit(0)
 
 def main():
-	if len(sys.argv) != 2 :
-		print_usage()
-	try:
-		model = LogisticRegression()
+    files = manage_arguments(sys.argv, 'train')
+    model = LogisticRegression(bias=False, verbose=True)
+    try:
+        data = pd.read_csv(sys.argv[1], index_col=0)
+        convert_str_data(data, 'Best Hand')
+    except:
+        print_usage('files', files[0])
 
-		data = pd.read_csv(sys.argv[1], index_col=0)
-
-		# Target (Y)
-		target = data['Hogwarts House']
-
-		# Classes (How many unique target)
-		classes = target.unique()
-
-		# Train and get results
-		weights = model.onevsall(data, target, classes)
-		weights.to_csv('./weights.csv')
-	except :
-		print('An error somewhere occured ! Reboot maybe ?')
-		pass
+    try:
+        # Target (Y)
+        target = data['Hogwarts House']
+        # Classes (How many unique target)
+        classes = target.unique()
+    except:
+        print_usage('data', files[0])
+    try:
+        # Train and get results
+        weights = model.onevsall(data, target, classes)
+        weights.to_csv('./weights.csv')
+    except:
+        print_usage('training')
 
 if __name__ == '__main__' :
-	main()
+    main()
